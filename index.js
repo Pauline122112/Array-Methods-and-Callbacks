79c0ed27a4412c2be01d610a661233b8ad52a9d2
Pauline122112 <pauline.design007@gmail.com>
2021-06-15 18:48:01 -0500

    difficult task 3 to complete

--- index.js
+++ index.js
@@ -43,11 +43,10 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
-}
-
-
+function getYears(array, getFinalsCB) {
+    const years = getFinalsCB(array).map(item => item.Year);
+    return years;
+  }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher-order function getWinners to do the following:  
